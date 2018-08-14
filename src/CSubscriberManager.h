673ac7ea5260ccf3551fc110476eb719a3520bc0
literofcola <literofcola@github.com>
2018-08-13 23:51:49 -0700

    SubscriberManager private->protected

--- src/CSubscriberManager.h
+++ src/CSubscriberManager.h
@@ -11,7 +11,7 @@ public:
 	std::string DebugPrintSubscribers();
     void NotifySubscribers();
 
-//private:
+protected:
 	struct SubscriberCount
 	{
 		SubscriberCount(Subscriber * l) : subscriber(l), refcount(1) {};
